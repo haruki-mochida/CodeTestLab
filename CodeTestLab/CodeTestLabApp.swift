5b7fab92fdfafdd34b9a13c9744edaa18785a4f8
haruki-mochida <motimotimotida@gmail.com>
2023-12-06 18:26:20 +0900

    Initial Commit

---
+++ CodeTestLab/CodeTestLabApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeTestLabApp.swift
+//  CodeTestLab
+//
+//  Created by 持田晴生 on 2023/12/06.
+//
+
+import SwiftUI
+
+@main
+struct CodeTestLabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
